47b1b71a2273e1453b8e17f4ba202bcdf5d7281e
leandro7w7 <leandrogimenez13579@gmail.com>
2025-05-28 18:06:25 -0300

    Clase 1- Variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+#numero enteros
+num1=10
+#Numeros flotaqntes
+flo1=10.5
+#String
+cadena="Hola, Python"
+#Booleano
+booleano=True
+
+numero=int(input("ingrese un numero: "))
+print(numero + str(num1))
+#--------------------------------
+#Listas
+#--------------------------------
+#git clone https://github.com/ListCodeName/CursoPythonConased.git
+#git fetch
+#git pull
